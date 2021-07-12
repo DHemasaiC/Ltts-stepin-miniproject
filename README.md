54e849c5a1cf690ce083bc01f27468c7e0eccca1
DHemasaiC <86188035+DHemasaiC@users.noreply.github.com>
2021-07-12 18:32:12 +0530

    change 2

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 ## Ltts-stepin-miniproject
 
 # This project is based on the game
-i am trying to change this problems
--- hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    printf("testing github\n");
-    printf("testing github \n");
-    printf("i am dum nightmare nightmare");
-    return 0;
-}
